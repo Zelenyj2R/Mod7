753d9d8fa7abb7b19c43a3e2d95089ceab81777d
Artem Knyzhnykov <111915140+Zelenyj2R@users.noreply.github.com>
2023-05-29 17:23:54 +0200

    Update HttpStatusChecker.java

--- src/main/java/module7/HttpStatusChecker.java
+++ src/main/java/module7/HttpStatusChecker.java
@@ -6,7 +6,7 @@ public class HttpStatusChecker {
             String url = "https://http.cat" + "/" + code + ".jpg";
 
             int statusCode = Utils.getRequestStatus(url);
-            if (statusCode == -1 || statusCode == 404) {
+            if (statusCode == 0 || statusCode == 404) {
                 throw new RuntimeException("Image not found");
             } else {
                 return url;
